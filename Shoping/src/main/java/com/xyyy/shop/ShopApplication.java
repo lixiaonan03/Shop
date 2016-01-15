fc57f721e3c1f9aac17060926f7dcd71c87544f2
lixiaonan03 <472379153@qq.com>
2016-01-15 14:57:39 +0800

    2.2 开发使用是主分支

--- Shoping/src/main/java/com/xyyy/shop/ShopApplication.java
+++ Shoping/src/main/java/com/xyyy/shop/ShopApplication.java
@@ -65,7 +65,7 @@ public class ShopApplication extends Application {
 		application = this;
 		PreferencesUtil.init(application);
 		BadHandler.getInstance().init(application);
-		StatService.setDebugOn(false);
+		StatService.setDebugOn(true);
 		//初始化数据库
 		DataBaseHelper.getHelper(application).getWritableDatabase();
 		
